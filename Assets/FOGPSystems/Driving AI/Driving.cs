db3b08a45a210d42399fa9df9eb03ec991b2ed91
CBWillhite <willhitecharles.mail@gmail.com>
2022-04-26 12:18:18 -0500

    Driving AI 1
    The first part of the driving AI.

---
+++ Assets/FOGPSystems/Driving AI/Driving.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Driving.AI
+{
+    public int maxSpeed = 100;
+
+    public class AICar : MonoBehavior
+    {
+        public Transform path;
+        private List<Transform> nodes;
+
+        void Start()
+        {
+            List<Transform> carPath = path.GetComponentsInChildren<Transform>();
+            nodes = new List<Transform>();
+
+            for (int i = 0; i < carPath.Length; i++)
+            {
+                if (carPath[i] != transform)
+                {
+                    nodes.Add(carPath[i]);
+                }
+            }
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/FOGPSystems/Driving AI/Path.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Driving.path
+{
+public class Path : MonoBehavior {
+	public Color acolor;
+
+	private List<Transform> nodes = new List<Transform>();
+
+	void OnDrawGizmosSelected()
+    {
+		Gizmos.color = acolor;
+
+		List<Transform> pathCreate = GetComponentsInChildren<Transform>();
+		nodes = new List<Transform>();
+
+		for(int i = 0; i < pathCreate.Length; i++)
+		{ 
+			if(pathCreate[i]!=transform)
+            {
+				nodes.Add(pathCreate[i]);
+            }
+		}
+
+		for(int i = 0; i < nodes.Count; i++)
+        {
+			Vector3 thisNode = nodes[i].position;
+			Vector3 prevNode;
+
+			if (i>0)
+            {
+				prevNode = nodes[i - 1].position;
+            }
+			else if (i==0 && nodes.Count > 1)
+            {
+				prevNode = nodes[nodes.Count - 1].position;
+			}
+
+			Gizmos.DrawLine(prevNode, thisNode);
+			Gizmos.DrawWireSphere(thisNode, 0.3f);
+        }
+	}
+}
+}
